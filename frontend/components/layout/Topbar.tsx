4c8bab21133e185fd733a0fc064c23ac4cc206fc
Ashu Sah <79694434+ashu38472@users.noreply.github.com>
2026-01-02 16:41:46 +0530

    Update Topbar.tsx

--- frontend/components/layout/Topbar.tsx
+++ frontend/components/layout/Topbar.tsx
@@ -124,7 +124,7 @@ export default function Topbar() {
             <div className="flex justify-end gap-3">
               <button
                 onClick={() => setConfirmLogout(false)}
-                className="px-4 py-2 rounded-lg border text-sm"
+                className="px-4 py-2 rounded-lg text-[#41414E] bg-[#F5F9FF] border text-sm"
               >
                 Cancel
               </button>
